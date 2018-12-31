e210ab4aa90e37347fbf567798ef3a9cca0bffcc
Barton <scott.barton@Dxc.com>
2018-12-31 14:17:48 +0000

    Advent of Code Day 2 2/2 Completed

--- Day2/Advent Of Code Day 2.ps1
+++ Day2/Advent Of Code Day 2.ps1
@@ -32,3 +32,27 @@ $Fabric | ForEach-Object{
 }
 #Display Total Fabric after all sums completed
 "Total Fabric $TotalFabric"
+
+
+#Part 2
+#get ribbon length
+
+#set length to 0
+$ribbonLength = 0
+#get input content
+$Fabric = Get-Content "$ENV:USERPROFILE\documents\GIT\powershell\AOC-2015\day2\fabric.txt"
+#filter through each line
+$Fabric | ForEach-Object {
+   #create an array of data
+   $array = @()
+   #add to the array each value from the length, width, height
+   $array += [int]$_.Split('x')[0]
+   $array += [int]$_.Split('x')[1]
+   $array += [int]$_.Split('x')[2]
+   #sort from smallest to largest values
+   $array = $array | Sort-Object
+   #find the length for each line of data and add it to ribbon length total
+   $ribbonLength += 2 * $array[0] + 2 * $array[1] + $array[0] * $array[1] * $array[2]
+}
+#print data
+"Total Ribbon: $ribbonLength"
